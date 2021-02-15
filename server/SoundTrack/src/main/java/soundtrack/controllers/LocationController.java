c15ac8740a88c85130e8632c7faafd1122af93e3
Kelton Holsen <keltonholsen@gmail.com>
2021-02-15 15:53:43 -0600

    My commit

--- server/SoundTrack/src/main/java/soundtrack/controllers/LocationController.java
+++ server/SoundTrack/src/main/java/soundtrack/controllers/LocationController.java
@@ -1,8 +1,13 @@
 package soundtrack.controllers;
 
 import org.springframework.stereotype.Controller;
+import soundtrack.domain.LocationService;
 
 @Controller
 public class LocationController {
-    
+    private final LocationService service;
+
+    public LocationController(LocationService service) {
+        this.service = service;
+    }
 }
---
+++ server/SoundTrack/src/test/java/soundtrack/data/LocationJdbcRepositoryTest.java
@@ -0,0 +1,9 @@
+package soundtrack.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationJdbcRepositoryTest {
+
+
+
+}
