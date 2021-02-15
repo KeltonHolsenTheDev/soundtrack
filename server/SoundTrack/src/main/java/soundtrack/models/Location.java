a4b025f67179c938566364c399afaaf688ec4ab9
Kelton Holsen <keltonholsen@gmail.com>
2021-02-15 14:08:32 -0600

    Made server

---
+++ server/SoundTrack/src/main/java/soundtrack/models/Location.java
@@ -0,0 +1,23 @@
+package soundtrack.models;
+
+public class Location {
+
+    private int locationId;
+    private String address;
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
