dad8e535ea699fc0820bb5fb4a17d903a758b4b1
torre382 <torre382@umn.edu>
2021-02-22 09:21:58 -0600

    Added tests to event test

--- server/sound-track/src/test/java/soundtrack/models/EventTest.java
+++ server/sound-track/src/test/java/soundtrack/models/EventTest.java
@@ -1,8 +1,51 @@
 package soundtrack.models;
 
+import org.assertj.core.util.VisibleForTesting;
 import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventTest {
 
+    @Test
+    void emptyEventNameShouldFail() {
+        Event event = new Event();
+        event.setEventName("   ");
 
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Event>> violations = validator.validate(event);
+
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    void nullEventNameShouldFail() {
+        Event event = new Event();
+        event.setEventName(null);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Event>> violations = validator.validate(event);
+
+        assertEquals(6, violations.size());
+    }
+
+    @Test
+    void emptyStartDateShouldFail() {
+        Event event = new Event();
+        event.setEventName("   ");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Event>> violations = validator.validate(event);
+
+        assertEquals(1, violations.size());
+    }
 }
