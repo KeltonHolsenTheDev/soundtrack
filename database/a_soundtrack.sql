2206eea9a5e708b08a24455aa39103d491b2e54f
torre382 <torre382@umn.edu>
2021-02-25 13:48:40 -0600

    updated the database.

--- database/a_soundtrack.sql
+++ database/a_soundtrack.sql
@@ -150,28 +150,25 @@ insert into item_type (type_name) values
 
 insert into item (item_name, description, brand, item_type_id, item_category, location_id, location_description, is_broken, notes) values
 		("Microphone 1", "Bass mic", "Sony", 1, "AUDIO", 1 , "Shelf A", false, "no notes"),
-        ("Drum", "Kick", "DrumstickInc", 2, "AUDIO", 1 , "Shelf B", true, "no notes"),
-        ("Short Throw", "Projector", "Panasonic", 3, "VIDEO", 1 , "Shelf C", false, "this one is good for short distance projecting");
-
-insert into item (item_name, description, brand, item_type_id, item_category, location_id, location_description, is_broken, notes) values
-		("Microphone 1", "Bass mic", "Sony", 1, "AUDIO", 1 , "Shelf A", false, "no notes"),
-        ("Drum", "Kick", "DrumstickInc", 2, "AUDIO", 1 , "Shelf B", false, "no notes"),
-        ("Short Throw", "Projector", "Panasonic", 3, "VIDEO", 1 , "Shelf C", false, "this one is good for short distance projecting");
+        ("Drum", "Kick", "DrumstickInc", 2, "AUDIO", 2 , "Shelf B", true, "no notes"),
+        ("Short Throw", "Projector", "Panasonic", 3, "VIDEO", 3 , "Shelf C", false, "this one is good for short distance projecting");
+        
+insert into system_user (first_name, last_name, email, phone, access_level, password_hash) values
+	("Admin", "Admin", "default@login.email", "555-555-5555", "ROLE_ADMINISTRATOR", "$2y$12$OW9tWlw1ECSWaBrZicW4qO6eAry1ZkDh9HAG9hy4WYLWUDtDDE/Ga");
         
 insert into user_role (user_id, role_id) values
 	(1, 1);
         
 insert into event_ (event_name, start_date, end_date, location_id, owner_id) values
-		("Church Service", '2021-02-21', '2021-02-21', 1, 1),
-        ("Rock Concert", '2021-02-24', '2021-02-25', 2, 1);
+		("Church Service", '2021-03-21', '2021-03-21', 1, 1),
+        ("Rock Concert", '2021-03-24', '2021-03-25', 2, 1);
         
 insert into event_item (event_id, item_id) values
     (1, 1),
     (2, 3);
     
 insert into event_user_role(event_id, user_role_id) values
-	(2, 3),
-    (1, 4);
+    (1, 1);
            
 select * from system_user;
 
